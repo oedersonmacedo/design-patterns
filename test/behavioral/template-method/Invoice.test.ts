d1e933a942f5faa8dd6dfd9fef8f20afcef81b5e
Ederson Macedo <ederson.mos@gmail.com>
2023-04-17 18:02:18 -0300

    Created news tests

--- test/behavioral/template-method/Invoice.test.ts
+++ test/behavioral/template-method/Invoice.test.ts
@@ -8,8 +8,27 @@ test("should to create a invoice and to calcultate tax", function () {
 	const invoice = new Invoice();
 	invoice.addItem(new Beer("Heineken 300ml", 10));
 	invoice.addItem(new Whisky("Jack Daniels", 100));
-	invoice.addItem(new Water("Santa Catarina 500ml", 5));
+	invoice.addItem(new Water("500ml", 5));
 	invoice.addItem(new Eletronic("TV", 1000));
 	const taxes = invoice.getTaxes();
 	expect(taxes).toBe(121);
 });
+
+test("should to create a invoice to calcultate without tax", function () {
+	const invoice = new Invoice();
+	invoice.addItem(new Water("500ml", 100));
+	const taxes = invoice.getTaxes();
+	expect(taxes).toBe(0);
+});
+
+
+test("should to generate error, because item water duplicate", function () {
+	const invoice = new Invoice();
+	invoice.addItem(new Beer("Heineken 300ml", 10));
+	invoice.addItem(new Whisky("Jack Daniels", 100));
+	invoice.addItem(new Water("500ml", 5));
+	invoice.addItem(new Eletronic("TV", 1000));
+	expect(()=> {
+		invoice.addItem(new Water("500ml", 100));
+	}).toThrow(new Error("Unable to insert a duplicate product!"));
+})
