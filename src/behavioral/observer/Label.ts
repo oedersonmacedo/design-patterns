17ece46af60ee69d15df150f84ad51263aec913c
Ederson Macedo <ederson.mos@gmail.com>
2023-04-24 16:44:43 -0300

    Created pattern of Observer

---
+++ src/behavioral/observer/Label.ts
@@ -0,0 +1,23 @@
+import Observer from "./Observer";
+
+export default class Label implements Observer {
+	private value: string;
+
+	constructor (readonly expression: string) {
+		this.value = "";
+	}
+
+	getValue () {
+		return this.value;
+	}
+
+	setValue (name: string, value: string) {
+		this.value = this.expression
+		.replace(`{{ ${name} }}`, value)
+		.replace(`{{${name}}}`, value);
+	}
+
+	update(name: string, value: string): void {
+		this.setValue(name, value);
+	}
+}
